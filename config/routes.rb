c80281e163ad098e8e733e8ff6327f70feca8143
sesaba23 <sesaba23@gmail.com>
2017-08-19 12:53:04 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  
   root 'static_pages#home'
   # This pattern routes a GET request for the URL /help
   # to the help action in the Static Pages controller
   get  '/help', to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
